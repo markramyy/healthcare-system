c30fd9ed236860142621ad72e5651700afe86a32
Mark Ramy <markramyy@gmail.com>
2025-04-23 15:15:48 +0200

    Enhance BaseViewSet by adding SearchFilter and OrderingFilter to filter_backends for improved query capabilities.

--- healthcare_ms/core/base_viewset.py
+++ healthcare_ms/core/base_viewset.py
@@ -3,7 +3,7 @@ from rest_framework.exceptions import APIException
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
-
+from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
 
 from healthcare_ms.core.pagination import StandardResultsSetPagination
@@ -23,4 +23,4 @@ class BaseViewSet(viewsets.GenericViewSet):
     authentication_classes = [SessionAuthentication, JWTAuthentication]
     permission_classes = [IsAuthenticated]
     pagination_class = StandardResultsSetPagination
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
